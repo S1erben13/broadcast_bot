313cca3455ea89b081a0f200a8c373d00be6c971
S1erben13 <asimovchannel@gmail.com>
2025-03-23 08:15:26 +0300

    updated models with Company and relations

--- api/models.py
+++ api/models.py
@@ -1,13 +1,39 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean
-from sqlalchemy.orm import Mapped
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, Enum, UniqueConstraint
+from sqlalchemy.orm import Mapped, relationship
 from database import Base
+from enum import Enum as PyEnum
 
-class Message(Base):
-    __tablename__ = 'message'
+
+class Role(PyEnum):
+    MASTER = "master"
+    SERVANT = "servant"
+
+
+class Company(Base):
+    __tablename__ = 'company'
 
     id: Mapped[int] = Column(Integer, primary_key=True, autoincrement=True)
-    author_id: Mapped[str] = Column(String)
-    text: Mapped[str] = Column(Text)
+    name: Mapped[str] = Column(String, nullable=False)
+
+    user_roles = relationship('UserCompanyRole', back_populates='company')
+
+
+class UserCompanyRole(Base):
+    __tablename__ = 'user_company_role'
+
+    id: Mapped[int] = Column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = Column(Integer, ForeignKey('user.id'), nullable=False)
+    company_id: Mapped[int] = Column(Integer, ForeignKey('company.id'), nullable=False)
+    role: Mapped[Role] = Column(Enum(Role), nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'company_id', 'role', name='uq_user_company_role'),
+    )
+
+    user = relationship('User', back_populates='company_roles')
+
+    company = relationship('Company', back_populates='user_roles')
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -18,6 +44,9 @@ class User(Base):
     last_message_id: Mapped[str] = Column(Integer)
     followed: Mapped[bool] = Column(Boolean, default=True)
 
+    company_roles = relationship('UserCompanyRole', back_populates='user')
+
+
 class Master(Base):
     __tablename__ = 'master'
 
@@ -25,3 +54,5 @@ class Master(Base):
     user_id: Mapped[str] = Column(String, unique=True)
     chat_id: Mapped[str] = Column(String, unique=True)
     active: Mapped[str] = Column(Boolean, default=True)
+
+    company_roles = relationship('UserCompanyRole', back_populates='user')
